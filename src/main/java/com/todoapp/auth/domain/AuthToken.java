4f739893d5a92c7188214bdb2b6abd4eaec7fde7
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-05-13 15:31:12 -0400

    add AuthToken class for handling authentication tokens

---
+++ src/main/java/com/todoapp/auth/domain/AuthToken.java
@@ -0,0 +1,11 @@
+package com.todoapp.auth.domain;
+
+public class AuthToken {
+    private final String token;
+
+    public AuthToken(String token) {
+        this.token = token;
+    }
+
+    public String getToken() { return token; }
+}
