3795766c102f72e3471c2ddaab0a2544c5dd851f
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-05-13 15:30:58 -0400

    add AuthController with login endpoint using LoginRequestDTO and AuthResponseDTO

--- src/main/java/com/todoapp/auth/adapter/in/AuthController.java
+++ src/main/java/com/todoapp/auth/adapter/in/AuthController.java
@@ -1,4 +1,23 @@
 package com.todoapp.auth.adapter.in;
 
+import com.todoapp.auth.dto.LoginRequestDTO;
+import com.todoapp.auth.dto.AuthResponseDTO;
+import com.todoapp.auth.port.in.AuthUseCase;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    private final AuthUseCase authUseCase;
+
+    public AuthController(AuthUseCase authUseCase) {
+        this.authUseCase = authUseCase;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginRequestDTO dto) {
+        return ResponseEntity.ok(authUseCase.login(dto));
+    }
 }
