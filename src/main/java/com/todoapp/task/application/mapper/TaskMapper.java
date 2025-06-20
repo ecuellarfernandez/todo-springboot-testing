077a316a1f8f16a7cb30866e915709c95cf08207
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-06-19 23:17:34 -0400

    feat: add TaskResponseDTO mapping method to TaskMapper

--- src/main/java/com/todoapp/task/application/mapper/TaskMapper.java
+++ src/main/java/com/todoapp/task/application/mapper/TaskMapper.java
@@ -2,10 +2,12 @@ package com.todoapp.task.application.mapper;
 
 import com.todoapp.task.adapter.out.TaskEntity;
 import com.todoapp.task.domain.Task;
+import com.todoapp.task.dto.TaskResponseDTO;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
     Task entityToDomain(TaskEntity taskEntity);
     TaskEntity domainToEntity(Task task);
+    TaskResponseDTO toResponseDTO(Task task);
 }
