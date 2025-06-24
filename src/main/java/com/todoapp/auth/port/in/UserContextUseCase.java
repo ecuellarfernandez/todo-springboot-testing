c96f2d3f0ba75ada9d976317fde935718effc1fd
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-06-24 08:25:49 -0400

    refactor: add method to retrieve current user information with UserMeResponseDTO

--- src/main/java/com/todoapp/auth/port/in/UserContextUseCase.java
+++ src/main/java/com/todoapp/auth/port/in/UserContextUseCase.java
@@ -1,7 +1,9 @@
 package com.todoapp.auth.port.in;
 
+import com.todoapp.auth.dto.UserMeResponseDTO;
 import com.todoapp.user.domain.User;
 
 public interface UserContextUseCase {
     User getCurrentUser(String token);
+    UserMeResponseDTO getCurrentUserInfo(String authHeader);
 }
