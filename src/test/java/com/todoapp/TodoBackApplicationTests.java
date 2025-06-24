8dd399e0561e2785076dfed081cb550aa95d089d
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-06-24 08:25:35 -0400

    fix: add JWT_SECRET property for testing in TodoBackApplicationTests

--- src/test/java/com/todoapp/TodoBackApplicationTests.java
+++ src/test/java/com/todoapp/TodoBackApplicationTests.java
@@ -2,8 +2,10 @@ package com.todoapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "JWT_SECRET=miSuperSecretoUltraLargo123@@12345678")
 class TodoBackApplicationTests {
 
     @Test
