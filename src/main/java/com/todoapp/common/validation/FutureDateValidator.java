b5f414bed366d153e56164f822607bbd980a7498
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-07-09 10:57:45 -0400

    feat: add FutureDate validation annotation and corresponding validator

---
+++ src/main/java/com/todoapp/common/validation/FutureDateValidator.java
@@ -0,0 +1,14 @@
+package com.todoapp.common.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+
+public class FutureDateValidator implements ConstraintValidator<FutureDate, LocalDate> {
+    @Override
+    public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
+        if (value == null) return true;
+        return value.isAfter(LocalDate.now());
+    }
+}
+
